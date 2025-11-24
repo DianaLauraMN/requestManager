353df8f053bc565a98b5a371e4a88ec2e3e83294
DianaLauraMN <dianaalauramn@gmail.com>
2025-11-24 13:36:56 -0700

    Logger and error management added in service layer

--- src/service/EventDetailService.ts
+++ src/service/EventDetailService.ts
@@ -3,7 +3,10 @@ import { PaginationEventFilterRequestDto } from "../dtos/EventDetailDTO/EventPag
 import { GetEventDetailResponseDto } from "../dtos/EventDetailDTO/GetEventDetailResponseDTO";
 import { UpdateEventDetailRequestDto } from "../dtos/EventDetailDTO/UpdateEventDetailRequestDTO";
 import EventDetailRepository from "../repository/EventDetailRepository";
+import { ErrorRequestMessage } from "../utilities/ErrorMessage";
 import { AppLogger } from "../utilities/Logger";
+import { AppError } from "../utilities/Prisma/PrismaErrorValidator";
+import { ErrorStatus } from "../utilities/StatusCode";
 
 class EventDetailService {
     private eventDetailRepository: EventDetailRepository;
@@ -23,12 +26,30 @@ class EventDetailService {
     }
 
     async addEventDetail(createEventDetailDto: CreateEventDetailRequestDto, userId: string): Promise<GetEventDetailResponseDto> {
+        if (!userId) {
+            this.appLogger.error(ErrorRequestMessage.INVALID_ID, ErrorStatus.BAD_REQUEST_ERROR, EventDetailService.name, this.addEventDetail.name);
+            throw new AppError(ErrorRequestMessage.INVALID_ID, ErrorStatus.BAD_REQUEST_ERROR);
+        }
         const eventDetail = await this.eventDetailRepository.addEventDetail(createEventDetailDto, userId);
+        if (!eventDetail) {
+            this.appLogger.error(ErrorRequestMessage.ADDING_REQUEST, ErrorStatus.BAD_REQUEST_ERROR, EventDetailService.name, this.getAllEventsDetials.name);
+            throw new AppError(ErrorRequestMessage.ADDING_REQUEST, ErrorStatus.BAD_REQUEST_ERROR);
+        }
+        this.appLogger.log(`Event of user with id ${userId} was added`, EventDetailService.name, this.addEventDetail.name);
         return eventDetail;
     }
 
     async updateEventDetail(updateEventDetailDto: UpdateEventDetailRequestDto, eventDetailId: number): Promise<GetEventDetailResponseDto> {
+        if (!eventDetailId) {
+            this.appLogger.error(ErrorRequestMessage.INVALID_ID, ErrorStatus.BAD_REQUEST_ERROR, EventDetailService.name, this.updateEventDetail.name);
+            throw new AppError(ErrorRequestMessage.INVALID_ID, ErrorStatus.BAD_REQUEST_ERROR);
+        }
         const eventDetailUpdated = await this.eventDetailRepository.updateEventDetail(updateEventDetailDto, eventDetailId);
+        if (!eventDetailUpdated) {
+            this.appLogger.error(ErrorRequestMessage.UPDATING_REQUEST, ErrorStatus.BAD_REQUEST_ERROR, EventDetailService.name, this.updateEventDetail.name);
+            throw new AppError(ErrorRequestMessage.UPDATING_REQUEST, ErrorStatus.BAD_REQUEST_ERROR);
+        }
+        this.appLogger.log(`Event detail with id ${eventDetailId} was updated`, EventDetailService.name, this.updateEventDetail.name);
         return eventDetailUpdated;
     }
 
--- src/service/UserService.ts
+++ src/service/UserService.ts
@@ -1,5 +1,5 @@
 import { PaginationResponse } from "../dtos/GetAllRequestsEntitiesResponseDTO";
-import { PaginationRequestDTO, UserProfilePaginationDto, UserProfilePaginationOrderBy } from "../dtos/PaginationRequestDTO";
+import { UserProfilePaginationDto, UserProfilePaginationOrderBy } from "../dtos/PaginationRequestDTO";
 import { GetProfileResponseDto } from "../dtos/ProfileDto/GetProfileResponseDTO";
 import { UpdateProfileRequestDto } from "../dtos/ProfileDto/UpdateProfileRequestDTO";
 import { GetUserResponseDto } from "../dtos/UserDto/GetUserResponseDto";
@@ -7,8 +7,11 @@ import { UpdateUserRequestDto } from "../dtos/UserDto/UpdateUserRequestDTO";
 import { PaginationUserFilterByDto, UserEventTypePaginationFilterBy, UserStatusPaginationFilterBy } from "../dtos/UserDto/UserPaginationRequestDTO";
 import { UserRole, UserStatus } from "../generated/prisma";
 import UserRepository from "../repository/UserRepository";
+import { ErrorRequestMessage } from "../utilities/ErrorMessage";
 import { AppLogger } from "../utilities/Logger";
+import { AppError } from "../utilities/Prisma/PrismaErrorValidator";
 import * as bcrypt from 'bcrypt';
+import { ErrorStatus } from "../utilities/StatusCode";
 
 class UserService {
     private userRepository: UserRepository = UserRepository.getInstance();
@@ -27,35 +30,71 @@ class UserService {
         return allUsersData;
     }
 
-    async addUser(requestId: string, email: string, tel: string) {
-        const { user, profile } = await this.userRepository.addUser({ email, tel, role: UserRole.CUSTOMER, fk_request_id: requestId, status: UserStatus.PENDING })
-    }
-
-    async updateUser(userUpdated: UpdateUserRequestDto, id: string) {
-        const user = await this.userRepository.updateUser(userUpdated, id);
-        return user;
-    }
-
-    async establishUserPassword(password: string, id: string) {
-        if (!password || !id) {
-            throw new Error('Error, invalid password');
-        }
-        const passwordHashed = await bcrypt.hash(password, 12);
-        const userUpdated = await this.userRepository.establishUserPassword(passwordHashed, id);
-        return userUpdated;
-    }
-
     async getAllUsersProfiles(userProfilePaginationDto: UserProfilePaginationDto): Promise<PaginationResponse<GetProfileResponseDto>> {
         const allUserProfilesData = await this.userRepository.getAllUsersProfilesOrderedBy(Number(userProfilePaginationDto.page), Number(userProfilePaginationDto.limit), (userProfilePaginationDto.orderBy as UserProfilePaginationOrderBy))
         const { data } = allUserProfilesData;
-        this.appLogger.log(data.length > 0 ? `Getting ${data.length} users` : "No users founded", UserService.name, this.getAllUsers.name);
+        this.appLogger.log(data.length > 0 ? `Getting ${data.length} users with profile` : "No users with profile founded", UserService.name, this.getAllUsersProfiles.name);
         return allUserProfilesData;
     }
 
+    async addUser(requestId: string, email: string, tel: string) {
+        const { user, profile } = await this.userRepository.addUser({ email, tel, role: UserRole.CUSTOMER, fk_request_id: requestId, status: UserStatus.PENDING });
+        if (!user || !profile) {
+            this.appLogger.error(ErrorRequestMessage.ADDING_REQUEST, ErrorStatus.BAD_REQUEST_ERROR, UserService.name, this.addUser.name);
+            throw new AppError(ErrorRequestMessage.ADDING_REQUEST, ErrorStatus.BAD_REQUEST_ERROR);
+        }
+        this.appLogger.log('New User with profile added', UserService.name, this.addUser.name);
+        return { user, profile };
+    }
+
+    async updateUser(userUpdated: UpdateUserRequestDto, userId: string) {
+        if (!userId) {
+            this.appLogger.error(ErrorRequestMessage.INVALID_ID, ErrorStatus.BAD_REQUEST_ERROR, UserService.name, this.updateUser.name);
+            throw new AppError(ErrorRequestMessage.INVALID_ID, ErrorStatus.BAD_REQUEST_ERROR);
+        }
+        const user = await this.userRepository.updateUser(userUpdated, userId);
+        if (!user) {
+            this.appLogger.error(ErrorRequestMessage.UPDATING_REQUEST, ErrorStatus.BAD_REQUEST_ERROR, UserService.name, this.updateUser.name);
+            throw new AppError(ErrorRequestMessage.UPDATING_REQUEST, ErrorStatus.BAD_REQUEST_ERROR);
+        }
+        this.appLogger.log(`User with id ${userId} updated`, UserService.name, this.updateUser.name);
+        return user;
+    }
+
     async updateUserProfile(userProfileUpdated: UpdateProfileRequestDto, userId: string): Promise<GetProfileResponseDto> {
+        if (!userId) {
+            this.appLogger.error(ErrorRequestMessage.INVALID_ID, ErrorStatus.BAD_REQUEST_ERROR, UserService.name, this.updateUserProfile.name);
+            throw new AppError(ErrorRequestMessage.INVALID_ID, ErrorStatus.BAD_REQUEST_ERROR);
+        }
         const userProfile = await this.userRepository.updateUserProfile(userProfileUpdated, userId);
+        if (!userProfile) {
+            this.appLogger.error(ErrorRequestMessage.UPDATING_REQUEST, ErrorStatus.BAD_REQUEST_ERROR, UserService.name, this.updateUserProfile.name);
+            throw new AppError(ErrorRequestMessage.UPDATING_REQUEST, ErrorStatus.BAD_REQUEST_ERROR);
+        }
+        this.appLogger.log(`Profile of user with id ${userId} was updated`, UserService.name, this.updateUserProfile.name);
         return userProfile;
     }
+
+    async establishUserPassword(password: string, userId: string) {
+        if (!userId) {
+            this.appLogger.error(ErrorRequestMessage.INVALID_ID, ErrorStatus.BAD_REQUEST_ERROR, UserService.name, this.establishUserPassword.name);
+            throw new AppError(ErrorRequestMessage.INVALID_ID, ErrorStatus.BAD_REQUEST_ERROR);
+        }
+        if (!password) {
+            this.appLogger.error("INVALID PASSWORD", ErrorStatus.BAD_REQUEST_ERROR, UserService.name, this.establishUserPassword.name);
+            throw new AppError("INVALID PASSWORD", ErrorStatus.BAD_REQUEST_ERROR);
+        }
+        const passwordHashed = await bcrypt.hash(password, 12);
+        const userUpdated = await this.userRepository.establishUserPassword(passwordHashed, userId);
+
+        if (!userUpdated) {
+            this.appLogger.error(ErrorRequestMessage.UPDATING_REQUEST, ErrorStatus.BAD_REQUEST_ERROR, UserService.name, this.establishUserPassword.name);
+            throw new AppError(ErrorRequestMessage.UPDATING_REQUEST, ErrorStatus.BAD_REQUEST_ERROR);
+        }
+        this.appLogger.log(`Password of user with id ${userId} established`, UserService.name, this.establishUserPassword.name);
+        return userUpdated;
+    }
+
 }
 
 export default UserService;
